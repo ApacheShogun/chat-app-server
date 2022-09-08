0caa074bb179346436db924f16f139d3e9ce0fdb
Kahil <kahlilkmassicot@gmail.com>
2022-09-08 17:32:06 -0400

    chat app backend

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+require('dotenv').config()
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = []
+
+io.on("connection", (socket) => {
+  console.log("deh user connected", socket.id);
+
+  socket.on("join_chat", (data) => {
+    socket.join(data.room);
+    console.log(`user ${data.username} has joined room ${data.room}`);
+    users.push(data)
+    console.log(users);
+    io.to(data.room).emit("user_connected", ` ${data.username} has connected`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("get_message", data);
+  });
+
+
+  socket.on("exit_chat", data => {
+    console.log(`${data.username} has disconnected`);
+    io.to(data.room).emit("user_disconnected", ` ${data.username} has disconnected`);
+})
+
+  socket.on("disconnect", () => {
+    console.log("deh user deconnected", socket.id);
+    
+  });
+});
+
+
+server.listen(process.env.PORT || 4000, () => {
+  console.log("yoo deh server running");
+});
